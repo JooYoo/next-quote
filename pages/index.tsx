9246aee4fcec972685a5f610c05b67f0370a54dc
jooyoo <jooyoo@outlook.com>
2021-11-19 05:37:32 +0100

    feat: add dark-mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,14 +7,41 @@ import styles from '../styles/Home.module.scss';
 import Quote from '../components/Quote';
 import { displayRdm } from '../util/utils';
 import { useEffect, useState } from 'react';
+import { useTheme } from 'next-themes';
+import { MoonIcon, SunIcon } from '@heroicons/react/solid';
 
 export default function Home({
   books,
   rdmQuote,
   rdmHighlight,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const { systemTheme, theme, setTheme } = useTheme();
   const [rQuote, setRQuote] = useState(rdmQuote);
   const [rHighlight, setRHighlight] = useState(rdmHighlight);
+
+  // dark/light-mode toggle
+  const renderThemeChnager = () => {
+    // get current system theme
+    const currentTheme = theme === 'system' ? systemTheme : theme;
+
+    if (currentTheme === 'dark') {
+      return (
+        <SunIcon
+          className="w-7 h-7"
+          role="button"
+          onClick={() => setTheme('light')}
+        />
+      );
+    } else {
+      return (
+        <MoonIcon
+          className="w-7 h-7"
+          role="button"
+          onClick={() => setTheme('dark')}
+        />
+      );
+    }
+  };
 
   // get init rdm
   useEffect(() => {
@@ -36,6 +63,8 @@ export default function Home({
       </Head>
 
       <main className={styles.main}>
+        {/* TODO: need a Header Component
+        {renderThemeChnager()} */}
         <Quote rdmHighlight={rHighlight} rdmQuote={rQuote} />
 
         <button
