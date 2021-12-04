7e7de4b95088ea32ceaf852722ba198a2e786988
jooyoo <jooyoo@outlook.com>
2021-12-04 07:01:07 +0100

    style: fix sticky

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar';
 const Layout = ({ children }: any) => {
   return (
     <div className="flex flex-col h-screen">
-      <div className="sticky top-0" style={{ position: 'sticky', top: '0' }}>
+      <div className="sticky top-0">
         <Navbar />
       </div>
       <div className="flex-1 overflow-y-auto">{children}</div>
