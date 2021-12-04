5e8b1c914cbf13cb805ceb2e3727a67488f7a682
jooyoo <jooyoo@outlook.com>
2021-12-04 05:29:33 +0100

    fix: navbar-nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,18 +6,22 @@ const Navbar = () => {
   return (
     <div className="flex flex-row gap-10 place-content-end py-4 px-7 border-b-2 border-gray-500 border-opacity-25 rounded-lg shadow-lg">
       <div className="flex flex-grow justify-between">
-        {/* <Link href="/"> */}
-        <span className="text-2xl" role="button">
-          ❝
-        </span>
-        {/* </Link> */}
+        <Link href="/">
+          <span className="text-2xl" role="button">
+            ❝
+          </span>
+        </Link>
         <div className="flex flex-row gap-5">
-          {/* <Link href="/"> */}
-          <BookmarkAltIcon className="w-7 h-7" role="button" />
-          {/* </Link> */}
-          {/* <Link href="/books"> */}
-          <ViewBoardsIcon className="w-7 h-7" role="button" />
-          {/* </Link> */}
+          <Link href="/">
+            <a>
+              <BookmarkAltIcon className="w-7 h-7" role="button" />
+            </a>
+          </Link>
+          <Link href="/books">
+            <a>
+              <ViewBoardsIcon className="w-7 h-7" role="button" />
+            </a>
+          </Link>
         </div>
       </div>
       <ThemeToggle />
--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -4,7 +4,9 @@ import Navbar from '../components/Navbar';
 const Layout = ({ children }: any) => {
   return (
     <div className="flex flex-col h-screen">
-      <Navbar />
+      <div className="sticky top-0">
+        <Navbar />
+      </div>
       <div className="flex-1 overflow-y-auto">{children}</div>
     </div>
   );
